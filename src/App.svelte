a9198efe68a19e76f7d553b34a162df2473c8b00
Fabyan Mikhael <fabyan.mikhael@ryerson.ca>
2022-07-06 18:08:51 -0400

    viewer counter

--- src/App.svelte
+++ src/App.svelte
@@ -4,21 +4,25 @@ import { createEmptyAudioTrack, createEmptyVideoTrack } from "./fake";
 import { copy } from "clipboard";
 
     var peer = new Peer()
-    let disabled = false;
-    let shareEnabled = false;
+    let viewers = 0
+    let disabled = false
+    let shareEnabled = false
     let ID: string = peer.id
     let joinID: string
-    let notification = "";
+    let notification = ""
     let video: any
 
     peer.on('open', id => {
             ID = id
             console.log(`connected to PeerServer!`)
             const autojoin = new URLSearchParams(window.location.search).get("autojoin");
+            disabled = true;
             if (autojoin){
                 console.log("found autojoin code, joining...");
                 joinID = autojoin;
                 join();
+            }else{
+                disabled = false;
             }
         }
     );
@@ -30,11 +34,17 @@ import { copy } from "clipboard";
         })
         video.srcObject = ScreenData;
         peer.on("call", (call) => {
-            console.log(`Answering a call from ${call.connectionId}`);
+            console.log(`Answering a call from ${call.connectionId}`)
+            call.answer(ScreenData)
+            viewers = viewers + 1
+            call.on("iceStateChanged", s => {
+                if (s == "disconnected"){
+                    viewers = viewers - 1
+                }
+            });
             
-            call.answer(ScreenData);
         })
-        shareEnabled = true;
+        shareEnabled = true
     }
     let join = async () => {     
         console.log(`joining ${joinID}`);
@@ -61,7 +71,7 @@ import { copy } from "clipboard";
     <!-- svelte-ignore a11y-media-has-caption -->
     <div>   
         <div style="width: 100%; text-align: center;">
-            <button hidden={!shareEnabled} on:click={()=>copy(`${window.location.origin}?autojoin=${ID}`)}>Share</button>
+            <button hidden={!shareEnabled} on:click={()=>copy(`${window.location.origin}?autojoin=${ID}`)}>Share | 👀 {viewers}</button>
         </div>
         <br>
         <h1>{notification}</h1>
@@ -78,7 +88,8 @@ import { copy } from "clipboard";
     }
     button{
         font-size: larger;
-        width: 6em;
+        width: 7em;
+        max-width: 10em;
         height: 2em;
         margin: 2px;
     }
